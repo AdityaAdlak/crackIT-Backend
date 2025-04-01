b36867126b66d2206465c86a4960e43d7953ff75
AdityaNFS <22070347@ycce.in>
2025-04-01 20:28:32 +0530

    final backend

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173", 
+        origin:[ "http://localhost:5173","https://crack-it-frontend.vercel.app"] ,
         credentials: true, 
     })
 );
